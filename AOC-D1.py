6bf7394f8039b39c4ca4d0533da2a45ff1ef4b50
A_R <u1175124@utah.edu>
2022-12-01 03:26:17 -0700

    first commit

---
+++ AOC-D1.py
@@ -0,0 +1,19 @@
+filename = "input.txt"
+
+with open(filename) as f:
+    content = f.read().splitlines()
+
+lst = []
+currentTotal = 0
+for line in content:
+    if line == '':
+        lst.append(currentTotal)
+        currentTotal = 0
+    else:
+        currentTotal = currentTotal + int(line)
+lst.sort(reverse=True)
+total = 0
+for topelf in range(3):
+    total += lst[topelf]
+
+print(total)
